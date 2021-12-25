a630a6e156221fd132fce9ba18e96aacc674c3be
Essam Fahmi <essamfahmi@ciandt.com>
2021-12-25 21:27:52 +0200

    Initial Commit

---
+++ iMovie/iMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMovieApp.swift
+//  iMovie
+//
+//  Created by Essam Fahmi on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct iMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
