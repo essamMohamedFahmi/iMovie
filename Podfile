a59116c483ab6d39d2920ee30faa1ad0058f3d3b
Essam Fahmi <essammohamedomran@gmail.com>
2021-12-26 17:43:02 +0200

    Create project high-level structure

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'iMovie' do
+
+  use_frameworks!
+  pod 'lottie-ios'
+
+end
---
+++ iMovie/Scenes/Splash/SplashView.swift
@@ -0,0 +1,34 @@
+//
+//  SplashView.swift
+//  iMovie
+//
+//  Created by Essam Fahmi on 25/12/2021.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+
+    // MARK: - Properties
+
+    private enum Strings {
+        static let spalshAnimationFileName = "splash_iMovie"
+    }
+
+    // MARK: - Body
+
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color.red, Color.black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.vertical)
+            .overlay(
+                LottieView(name: Strings.spalshAnimationFileName)
+                    .padding([.leading, .trailing], 30.0)
+            )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
