b166d69c0a4dc5b4d2a389f98b33e291a0eafacf
Morsy Mohamed <morsysoker@gmail.com>
2021-12-26 22:49:31 +0200

    Add app colors

---
+++ iMovie/Extensions/Color+App.swift
@@ -0,0 +1,18 @@
+//
+//  Color+App.swift
+//  iMovie
+//
+//  Created by MorsyElsokary on 26/12/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    // MARK: - Properties
+    
+    static let iMoviePurpure = Color("iMoviePurpure")
+    static let iMovieBlue = Color("iMovieBlue")
+    static let iMovieDarkBlue = Color("iMovieDarkBlue")
+    static let iMovieRose = Color("iMovieRose")
+}
