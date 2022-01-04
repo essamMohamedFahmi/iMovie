30324e471196a6d52296ce9bb9e79778c5794d6d
Morsy Mohamed <morsysoker@gmail.com>
2022-01-04 14:14:16 +0200

    Refactor ProfileView

---
+++ iMovie/Extensions/UINavigationController+App.swift
@@ -0,0 +1,23 @@
+//
+//  UINavigationController+App.swift
+//  iMovie
+//
+//  Created by MorsyElsokary on 04/01/2022.
+//
+
+import SwiftUI
+
+extension UINavigationController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white, .font: UIFont(name: "Poppins Bold", size: 40)! ]
+        navBarAppearance.configureWithTransparentBackground()
+        
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        UINavigationBar.appearance().compactAppearance = navBarAppearance
+    }
+}
