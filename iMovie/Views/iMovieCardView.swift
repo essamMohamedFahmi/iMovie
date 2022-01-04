96fe33737adcaaf3b635fe83581d1978a71a36c9
Morsy Mohamed <morsysoker@gmail.com>
2022-01-04 23:01:26 +0200

    Add RatingCircleView

--- iMovie/Views/iMovieCardView.swift
+++ iMovie/Views/iMovieCardView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct iMovieCardView: View {
+    
+    // MARK: - Properties
+    
+    var moviePoster: String
+    var movieTitle: String
+    var movieReleaseDate: String
+    var rateColor: Color
+    
     var body: some View {
         ZStack {
             VStack(spacing: 30) {
-                Image("MoviePoster")
+                Image(moviePoster)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 150, height: 225)
@@ -24,19 +32,19 @@ struct iMovieCardView: View {
                                               opacity: 0.1), lineWidth: 1))
                 
                 VStack(alignment: .leading) {
-                    Text("The Good Doctor")
+                    Text(movieTitle)
                         .font(.iMovieBold(15))
                         .lineLimit(3)
                         .minimumScaleFactor(0.5)
                     
-                    Text("Sep 25, 2017")
-                        .font(.iMovieLight(15))
+                    Text(movieReleaseDate)
+                        .font(.iMovieRegular(15))
                         .minimumScaleFactor(0.5)
                         .foregroundColor(.gray)
                 }
             }
             
-            LoadingProgress()
+            RatingCircle(rateColor: rateColor)
                 .offset(x: -30, y: 75)
         }
     }
@@ -44,49 +52,12 @@ struct iMovieCardView: View {
 
 struct iMovieCardView_Previews: PreviewProvider {
     static var previews: some View {
-        iMovieCardView()
+        iMovieCardView(moviePoster: "MoviePoster",
+                       movieTitle: "The Good Doctor",
+                       movieReleaseDate: "Sep 25,2017",
+                       rateColor: .iMovieRose)
     }
 }
 
-// MARK: Loading Progress
 
-struct LoadingProgress: View {
-    
-    @State private var isLoading = false
-    @State private var progress: CGFloat = 0.75
-    
-    var body: some View {
-        ZStack {
-            Circle()
-                .foregroundColor(.black)
-                .frame(width: 60, height: 60)
-            
-            HStack(spacing: 0) {
-                Text("\(Int(progress * 100))")
-                    .font(.iMovieBold(15))
-                    .foregroundColor(.white)
-                    .overlay(
-                        Text("%")
-                            .font(.iMovieBold(10))
-                            .foregroundColor(.white)
-                            .offset(x: 12, y: -6)
-                    )
-            }
-            .padding()
-            
-            Circle()
-                .stroke(Color.iMovieRose.opacity(0.6), lineWidth: 5)
-                .frame(width: 50, height: 50)
-            
-            Circle()
-                .trim(from: 0, to: isLoading ? progress : 0)
-                .stroke(Color.iMovieRose, lineWidth: 3)
-                .frame(width: 50, height: 50)
-                .rotationEffect(Angle(degrees: -90))
-                .animation(Animation.default.delay(1.0), value: isLoading)
-        }
-        .onAppear {
-            isLoading.toggle()
-        }
-    }
-}
+
